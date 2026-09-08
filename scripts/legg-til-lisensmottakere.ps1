e2a76f388a3f3572a236b1199bb54cd4e63263d8
Nour Khalil Rash <nkrush85@gmail.com>
2026-09-08 14:36:36 +0200

    Skill mellom manglende innlogging og manglende gruppe

--- scripts/legg-til-lisensmottakere.ps1
+++ scripts/legg-til-lisensmottakere.ps1
@@ -1,8 +1,17 @@
-Connect-MgGraph -TenantId "nordvikmarine.onmicrosoft.com" `
-                -Scopes "GroupMember.ReadWrite.All", "User.Read.All", "Group.Read.All"
+if (-not (Get-MgContext)) {
+    Connect-MgGraph -TenantId "nordvikmarine.onmicrosoft.com" `
+                    -Scopes "GroupMember.ReadWrite.All", "User.Read.All", "Group.Read.All"
+}
+
+$kontekst = Get-MgContext
+if (-not $kontekst) { throw "Ikke tilkoblet Graph. Kjør Connect-MgGraph først." }
+Write-Output "Tilkoblet som $($kontekst.Account) i $($kontekst.TenantId)"
 
 $gruppe = Get-MgGroup -Filter "displayName eq 'LIC-E3'"
-if (-not $gruppe) { throw "Fant ingen gruppe som heter LIC-E3" }
+if (-not $gruppe) {
+    $finnes = Get-MgGroup -Filter "startsWith(displayName,'LIC')" | ForEach-Object { $_.DisplayName }
+    throw "Fant ingen gruppe LIC-E3. Grupper som starter med LIC: $($finnes -join ', ')"
+}
 
 $upn = Import-Csv .\data\lisensmottakere.csv | ForEach-Object { $_.Brukernavn }
 Write-Output "Legger til $($upn.Count) brukere i $($gruppe.DisplayName)"
