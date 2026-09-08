341da3c445791ff8adb2f4c6d9bc7edb9c51cf83
Nour Khalil Rash <nkrush85@gmail.com>
2026-09-08 14:29:12 +0200

    Skript for å legge lisensmottakerne inn i LIC-E3

---
+++ scripts/legg-til-lisensmottakere.ps1
@@ -0,0 +1,21 @@
+Connect-MgGraph -Scopes "GroupMember.ReadWrite.All", "User.Read.All", "Group.Read.All"
+
+$gruppe = Get-MgGroup -Filter "displayName eq 'LIC-E3'"
+if (-not $gruppe) { throw "Fant ingen gruppe som heter LIC-E3" }
+
+$upn = Import-Csv .\data\lisensmottakere.csv | ForEach-Object { $_.Brukernavn }
+Write-Output "Legger til $($upn.Count) brukere i $($gruppe.DisplayName)"
+
+foreach ($u in $upn) {
+    try {
+        $bruker = Get-MgUser -UserId $u -ErrorAction Stop
+        New-MgGroupMember -GroupId $gruppe.Id -DirectoryObjectId $bruker.Id -ErrorAction Stop
+        Write-Output "  lagt til   $u"
+    }
+    catch {
+        Write-Output "  hoppet over $u  ($($_.Exception.Message))"
+    }
+}
+
+$antall = (Get-MgGroupMember -GroupId $gruppe.Id -All).Count
+Write-Output "LIC-E3 har na $antall medlemmer"
